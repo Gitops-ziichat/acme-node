ed51a53260a8f7ca436193d4a27e4f44582a19ca
lephuocthanh2000 <lephuocthanhnbk10h@gmail.com>
2025-06-19 15:03:28 +0700

    update

--- src/routes/v1/greetingRoutes.js
+++ src/routes/v1/greetingRoutes.js
@@ -4,6 +4,6 @@ import { greet } from "../../controllers/v1/greetingController.js";
 
 const router = Router();
 
-router.get("/hi", greet);
+router.get("/hello123", greet);
 
 export default router;
